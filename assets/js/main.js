11c11ffc0453e634a0f35ee9c483a9d48f51d331
hemlatajadhav <jadhavhema48@gmail.com>
2021-11-29 18:58:28 +0530

    Commit : Travel Site

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+﻿const readmorebtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text');
+readmorebtn.addEventListener('click', (e) => {
+    text.classList.toggle('show-more');
+    if (readmorebtn.innerText === 'Read More') {
+        readmorebtn.innerText = 'Read Less';
+
+    }
+    else {
+        readmorebtn.innerText = 'Read More';
+
+    }
+})
+
+const readmorebtn1 = document.querySelector('.read-more-btn1');
+const text = document.querySelector('.text1');
+readmorebtn1.addEventListener('click', (e) => {
+    text.classList.toggle('show-more');
+    if (readmorebtn1.innerText === 'Read More') {
+        readmorebtn1.innerText = 'Read Less';
+
+    }
+    else {
+        readmorebtn1.innerText = 'Read More';
+
+    }
+})
